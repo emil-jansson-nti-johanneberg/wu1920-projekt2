b37d525e3fa912236e50e6541bb5ea55847d1f52
emil-jansson-nti-johanneberg <emil.jansson@elev.ga.ntig.se>
2020-03-04 11:00:41 +0100

    Lektion Vecka 10

--- js/index.js
+++ js/index.js
@@ -1,8 +1,40 @@
-function myFunction() {
+function One() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
+  }
+}
+
+function Two() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Läs mer";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Läs mindre";
+    moreText.style.display = "inline";
+  }
+}
+
+function Three() {
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("2myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Läs mer";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Läs mindre";
+    moreText.style.display = "inline";
   }
 }
